bef3d5965ccaa306635b418b0d7c6579196f4098
richardbruskowski <r.bruskowski@sp.design>
2019-10-09 12:32:22 +0200

    Add segment controls (includes breaking changes!)
    + Some minor bugfix
    + Control progress by percentage instead of frame count

--- enhanced-lottie.framerfx/code/ScrollApp.tsx
+++ enhanced-lottie.framerfx/code/ScrollApp.tsx
@@ -1,24 +1,25 @@
-import { Override, Data, transform } from 'framer'
+import { Override, Data, transform } from "framer"
 
 // Override Docs: https://framer.com/docs/overrides
-window.log = console.log
+const log = console.log
 
 const data = Data({
-  currentFrame: 0,
+    currentFrame: 0,
 })
 
 export function Test(): Override {
-  return {
-    currentFrame: data.currentFrame,
-  }
+    return {
+        percentage: data.currentFrame,
+    }
 }
 
 export function MoveFrameScroll(): Override {
-  return {
-    onScroll(e) {
-      var move = e.point.y
-      log('move', move)
-      data.currentFrame = transform(move, [0, -2094], [0, 36])
-    },
-  }
+    return {
+        onScroll(info) {
+            //@ts-ignore
+            var move = info.point.y
+            log("move", move)
+            data.currentFrame = transform(move, [0, -1700], [0, 100])
+        },
+    }
 }
--- enhanced-lottie.framerfx/code/TestApp.tsx
+++ enhanced-lottie.framerfx/code/TestApp.tsx
@@ -1,56 +1,56 @@
-import { Override, Data, transform } from 'framer'
+import { Override, Data, transform } from "framer"
 
 // Override Docs: https://framer.com/docs/overrides
-window.log = console.log
+const log = console.log
 
 const data = Data({
-  state: 'play',
-  currentFrame: 20,
+    state: "play",
+    currentFrame: 20,
 })
 
 export function Test(): Override {
-  return {
-    currentFrame: data.currentFrame,
-    playState: data.state,
-  }
+    return {
+        percentage: data.currentFrame,
+        playState: data.state,
+    }
 }
 
 export function Play(): Override {
-  return {
-    onTap() {
-      data.state = 'play'
-    },
-  }
+    return {
+        onTap() {
+            data.state = "play"
+        },
+    }
 }
 
 export function Pause(): Override {
-  return {
-    onTap() {
-      data.state = 'pause'
-    },
-  }
+    return {
+        onTap() {
+            data.state = "pause"
+        },
+    }
 }
 
 export function Stop(): Override {
-  return {
-    onTap() {
-      data.state = 'stop'
-      data.currentFrame = 0
-    },
-  }
+    return {
+        onTap() {
+            data.state = "stop"
+            data.currentFrame = 0
+        },
+    }
 }
 
 export function MoveFrame(): Override {
-  return {
-    onTap() {
-      data.currentFrame -= 5
-    },
-  }
+    return {
+        onTap() {
+            data.currentFrame -= 5
+        },
+    }
 }
 export function MoveFrameSlider(): Override {
-  return {
-    onValueChange(value) {
-      data.currentFrame = value
-    },
-  }
+    return {
+        onValueChange(value) {
+            data.currentFrame = value
+        },
+    }
 }
